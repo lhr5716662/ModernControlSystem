149b2651495131902385f93d6744429bfc837733
lhr5716662 <57390645+lhr5716662@users.noreply.github.com>
2020-03-16 02:20:00 +0800

    all file

---
+++ a.m
@@ -0,0 +1,24 @@
+%This is the example from Page 536
+if 0
+	num = 5*[0.1 1];
+	f1 = [1 0]; f2 = [0.5 1]; f3 = [1/2500 0.6/50 1];
+	den = conv(f1, conv(f2, f3));
+	sys = tf(num, den)
+	bode(sys)
+end
+%This is the example from Page 622
+if 0
+	num = [0.5]; 
+	den = [1 2 1 0.5];
+	sys = tf(num, den);
+	nyquist(sys);
+end
+num = 5*[0.1 1];
+f1 = [1 0]; f2 = [0.5 1]; f3 = [1/2500 0.6/50 1];
+den = conv(f1, conv(f2, f3));
+sys = tf(num, den)
+figure(1)
+bode(sys);
+figure(2)
+nyquist(sys);
+
