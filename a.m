356643db6622c707f7e8cae467d987306d7b2411
lhr5716662 <57390645+lhr5716662@users.noreply.github.com>
2020-03-17 19:37:09 +0800

    iiiii

--- a.m
+++ a.m
@@ -13,12 +13,22 @@ if 0
 	sys = tf(num, den);
 	nyquist(sys);
 end
-num = 5*[0.1 1];
-f1 = [1 0]; f2 = [0.5 1]; f3 = [1/2500 0.6/50 1];
-den = conv(f1, conv(f2, f3));
-sys = tf(num, den)
-figure(1)
-bode(sys);
-figure(2)
-nyquist(sys);
+%num = 5*[0.1 1];
+%f1 = [1 0]; f2 = [0.5 1]; f3 = [1/2500 0.6/50 1];
+%den = conv(f1, conv(f2, f3));
+%sys = tf(num, den)
+z = [];
+p = [0, -2, -5];
+k = 50;
+sys = zpk(z, p, k);
 
+sysc = zpk([0], [-1] ,1);
+syst = series(sys, sysc);
+
+
+%figure(4)
+%bode(syst);
+%figure(5)
+%nyquist(syst);
+figure(7)
+margin(syst)
